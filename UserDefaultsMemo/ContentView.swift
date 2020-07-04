771d582d73297defb4f1f0b1d1936f28104a814e
Ryota <mathmyg@gmail.com>
2020-07-04 12:40:32 +0900

    Initial Commit

---
+++ UserDefaultsMemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UserDefaultsMemo
+//
+//  Created by Ryota on 2020/07/04.
+//  Copyright © 2020 Ryota. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
